977f1c2f001cafa3f65bba59164d0a3ec8d9bf48
boumrz <boumrz@gmail.com>
2025-06-30 23:42:54 +0300

    wip: ratio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -331,7 +331,7 @@ function App() {
                 image={cropImage}
                 crop={crop}
                 zoom={zoom}
-                aspect={0.8}
+                aspect={0.625}
                 onCropChange={setCrop}
                 onZoomChange={setZoom}
                 onCropComplete={onCropComplete}
